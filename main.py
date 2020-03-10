b63b82c1ec3f5dfd0c3ee58edaafe16b07bcd07e
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-10 16:49:11 +0900

    Tweak

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ tree = ET.parse('yahoo-news-rss/news-202003042125.xml')
 # get top elements
 root = tree.getroot()
 
-path = 'text/outpu.txt'
+path = 'output/outpu.txt'
 with open(path,mode='a') as f:
     for child in root.iter(tag='title'):
         f.write(child.text+"\n")
