f0f131dd357e2a6503cf724981885f32224cdd09
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-03-11 21:45:04 +0900

    add url

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import xml.etree.ElementTree as ET
 
 # read XMLdata
-tree = ET.parse('yahoo-news-rss/news-202003042125.xml')
-# get top elements
+tree = ET.parse('yahoo-news-rss/yahoo-rss.xml')
 root = tree.getroot()
 
 path = 'output/outpu.txt'
-with open(path,mode='a') as f:
-    for child in root.iter(tag='title'):
-        f.write(child.text+"\n")
+
+with open(path, mode='a') as f:
+    for child in tree.findall('.//item'):
+        f.write(child.find('title').text+'\n')
+        f.write(child.find('link').text+'\n')
 f.close()
--- yahoo-news.sh
+++ yahoo-news.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-date=$(date +'%Y%m%d%H%M')
-dirname="/home/vagrant/workspace/yahoo-news/yahoo-new-rss"
+# date=$(date +'%Y%m%d%H%M')
+dirname="/home/vagrant/workspace/yahoo-news/yahoo-news-rss"
 mkdir -p $dirname
-filename="$dirname/news-$date.xml"
+filename="$dirname/yahoo-rss.xml"
 echo "save to $filename"
 curl -s -o $filename -H "User-Agent: CrawlBot; awsedrft039@gmail.com" https://news.yahoo.co.jp/pickup/economy/rss.xml
